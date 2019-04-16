bde5a43cf3a16dff5b4c2f126554239105bda5db
FujiyamaYuta <yuta.fujiwara1029@gmail.com>
2019-04-16 10:24:00 +0900

    firstcommit

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,65 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "YutaFujiwara",
+      message: "Hey Flutter, You are so amazing !",
+      time: "15:30",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+  new ChatModel(
+      name: "YutaFujiwara",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+  new ChatModel(
+      name: "YutaFujiwara",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+  new ChatModel(
+      name: "YutaFujiwara",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+  new ChatModel(
+      name: "YutaFujiwara",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+  new ChatModel(
+      name: "YutaFujiwara",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+    new ChatModel(
+      name: "YutaFujiwara",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+  new ChatModel(
+      name: "YutaFujiwara",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+  new ChatModel(
+      name: "YutaFujiwara",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "https://ohmygod.jp/assets/img/fujiwara.jpg"),
+];
