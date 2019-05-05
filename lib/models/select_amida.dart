0952a4f30b7d6dc94ea6a7737824269dc9637b6e
FujiyamaYuta <yuta.fujiwara1029@gmail.com>
2019-05-06 08:55:50 +0900

    トグルを追加

---
+++ lib/models/select_amida.dart
@@ -0,0 +1,11 @@
+class SelectAmida {
+
+  /* 
+    1：あみだくじ
+    2：当たりくじ
+    3：
+  */
+  final int userNumber;
+
+  SelectAmida({this.userNumber});
+}
--- lib/pages/status_screen.dart
+++ lib/pages/status_screen.dart
@@ -2,65 +2,69 @@ import 'package:flutter/material.dart';
 import '../models/user_model.dart';
 
 
-class StatusScreen extends StatelessWidget{
-
-  final title = "Flluter サンプル";
-
-  @override
-  Widget build(BuildContext context){
-    return new MaterialApp(
-      title:'Flutter',
-      theme: new ThemeData(
-        primarySwatch:Colors.blue,
-      ),
-      home:new MyHomePage(
-        title:this.title,
-      )
-    );
-  }
+void main(){
+  runApp(new MaterialApp(
+    home: new StatusScreen(),
+  ));
 }
 
-class MyHomePage extends StatefulWidget{
-  MyHomePage({Key key, this.title}):super(key:key);
-  final String title;
-  // final String message;
-
+class StatusScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _State createState() => new _State();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+//State is information of the application that can change over time or when some actions are taken.
+class _State extends State<StatusScreen>{
 
-  int _counter = 0;
+  bool _value1 = true;
+  bool _value2 = false;
 
-  void _incrementCounter(){
+  void _onChanged1(bool value){
     setState((){
-      _counter++;
+      if(value == false){
+        return;
+      }
+      _value1 = value;
+      _value2 = false;
     });
   }
 
+  void _onChanged2(bool value){
+    setState((){
+      if(value == false){
+        return;
+      }
+      _value1 = false;
+      _value2 = value;
+    });
+  }
+
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      // appBar: AppBar(title:Text(widget.title)),
-      body:Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:<Widget>[
-            Text(
-              'You have pushed the button this many times'
-            ),
-            Text(
-              '$_counter',
-              style:Theme.of(context).textTheme.display1,
-            )
-          ]
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'set message',
-        child: Icon(Icons.add),
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      // appBar: new AppBar(
+      //   title: new Text('Name here'),
+      // ),
+      //hit Ctrl+space in intellij to know what are the options you can use in flutter widgets
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new SwitchListTile(
+                value: _value1, 
+                onChanged: _onChanged1,
+                title: new Text('あみだくじ', style: new TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              new SwitchListTile(
+                  value: _value2,
+                  onChanged: _onChanged2,
+                  title: new Text('当たりくじ', style: new TextStyle(fontWeight: FontWeight.bold)),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
